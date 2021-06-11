6337b860741ab2e234bc8a1f14c42896bd6b464e
gyoriz90 <64286754+gyoriz90@users.noreply.github.com>
2021-06-11 19:05:18 +0200

    All factory finished

---
+++ progtech_2021_spring/HalogenBulbFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace progtech_2021_spring
+{
+    class HalogenBulbFactory:BulbFactory
+    {
+        private int _bulbPower;
+        private int _bulbSize;
+
+        public HalogenBulbFactory(int bulbPower, int bulbSize)
+        {
+            _bulbPower = bulbPower;
+            _bulbSize = bulbSize;
+        }
+        public override Bulb GetBulb()
+        {
+            return new HalogenBulb(_bulbPower, _bulbSize);
+        }
+    }
+}
---
+++ progtech_2021_spring/NormalBulbFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace progtech_2021_spring
+{
+    class NormalBulbFactory:BulbFactory
+    {
+        private int _bulbPower;
+        private int _bulbSize;
+
+        public NormalBulbFactory(int bulbPower, int bulbSize)
+        {
+            _bulbPower = bulbPower;
+            _bulbSize = bulbSize;
+        }
+        public override Bulb GetBulb()
+        {
+            return new NormalBulb(_bulbPower, _bulbSize);
+        }
+    }
+}

2a81ca3b48c3e45b204dc54c420d85ef79c4e24c
gyoriz90 <64286754+gyoriz90@users.noreply.github.com>
2021-06-11 19:14:47 +0200

    all features done

--- progtech_2021_spring/Program.cs
+++ progtech_2021_spring/Program.cs
@@ -10,6 +10,27 @@ namespace progtech_2021_spring
     {
         static void Main(string[] args)
         {
+            BulbFactory factory = null;
+            Console.WriteLine("Add the type of bulb you would like to see: ");
+            string temp = Console.ReadLine();
+
+            switch (temp.ToLower())
+            {
+                case "ledbulb":
+                    factory = new LedBulbFactory(20, 2);
+                    break;
+                case "halogenbulb":
+                    factory = new HalogenBulbFactory(50, 5);
+                    break;
+                case "normalbulb":
+                    factory = new NormalBulbFactory(100, 10);
+                    break;
+            }
+
+            Bulb bulb = factory.GetBulb();
+            Console.WriteLine("Your buld details are below:");
+            Console.WriteLine("Bulb type: {0}\nBulb Power: {1}\nBulb Size: {2}",bulb.BulbType,bulb.BulbPower, bulb.BulbSize);
+            Console.ReadKey();
         }
     }
 }
